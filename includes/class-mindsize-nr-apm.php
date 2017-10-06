e0fbe65cd310ac8fe87e9595ce95efa3029f91e5
Gabor Javorszky <gabor@javorszky.co.uk>
2017-10-06 23:24:05 +0100

    Move config method

--- includes/class-mindsize-nr-apm.php
+++ includes/class-mindsize-nr-apm.php
@@ -68,6 +68,26 @@ class APM {
 	}
 
 	/**
+	 * Setup the default config variables for our request.
+	 */
+	public function config() {
+		$this->config = apply_filters( 'mindsize_nr_default_config', $this->get_default_config() );
+
+		if ( ! $this->config ) {
+			return;
+		}
+
+		if ( isset( $this->config['newrelic.appname'] ) && function_exists( 'newrelic_set_appname' ) ) {
+			newrelic_set_appname( $this->config['newrelic.appname'] );
+		}
+		if ( isset( $this->config['newrelic.capture_params'] ) && function_exists( 'newrelic_capture_params' ) ) {
+			newrelic_capture_params( $this->config['newrelic.capture_params'] );
+		}
+
+		do_action( 'mindsize_nr_setup_config', $this->config );
+	}
+
+	/**
 	 * Ajax and Cron requests should not have the Browser extension
 	 */
 	private function maybe_disable_autorum() {
@@ -131,26 +151,6 @@ class APM {
 			'newrelic.appname' => $this->get_appname(),
 			'newrelic.capture_params' => $this->plugin->helper->get_capture_url(),
 		];
-	}
-
-	/**
-	 * Setup the default config variables for our request.
-	 */
-	public function config() {
-		$this->config = apply_filters( 'mindsize_nr_default_config', $this->get_default_config() );
-
-		if ( ! $this->config ) {
-			return;
-		}
-
-		if ( isset( $this->config['newrelic.appname'] ) && function_exists( 'newrelic_set_appname' ) ) {
-			newrelic_set_appname( $this->config['newrelic.appname'] );
-		}
-		if ( isset( $this->config['newrelic.capture_params'] ) && function_exists( 'newrelic_capture_params' ) ) {
-			newrelic_capture_params( $this->config['newrelic.capture_params'] );
-		}
-
-		do_action( 'mindsize_nr_setup_config', $this->config );
 	}
 
 	/**
