58c6867df24ffce48f5c50b564f192686ee12ac0
Gabor Javorszky <gabor@javorszky.co.uk>
2018-02-10 14:14:08 +0000

    Bump version to 1.3.3, add changelog.txt

--- mindsize-newrelic.php
+++ mindsize-newrelic.php
@@ -10,14 +10,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Description:          Better WordPress data for New Relic, with WooCommerce support.
  * Author:               Mindsize
  * Author URI:           https://mindsize.me
- * Version:              1.3.2
+ * Version:              1.3.3
  * Requires at least:    4.4
- * Tested up to:         4.9.2
+ * Tested up to:         4.9.4
  * WC requires at least: 2.6.0
  * WC tested up to:      3.2.0
  */
 
-define( 'MINDSIZE_NR_VERSION', '1.3.2' );
+define( 'MINDSIZE_NR_VERSION', '1.3.3' );
 define( 'MINDSIZE_NR_SLUG', 'mindsize-newrelic' );
 define( 'MINDSIZE_NR_FILE', __FILE__ );
 define( 'MINDSIZE_NR_DIR', plugin_dir_path( MINDSIZE_NR_FILE ) );
